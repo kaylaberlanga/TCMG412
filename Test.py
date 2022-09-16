d3683b29436634e69c3bc4e61df70b7ab5efb3cd
kaylaberlanga <kaylaberlanga25@gmail.com>
2022-09-16 18:01:25 -0500

    new test

--- Test.py
+++ Test.py
@@ -1,12 +1,4 @@
-with open(r"412Project3.txt", 'r') as fp:
-    count = 0
-    for line in fp:
-        if line != "\n":
-            count += 1
-
-print('Total Number of Lines', count)
-
-words = ["Oct", "Sep", "Aug", "Jul", "Jun", "May", "Apr", "1995"]
+words = ["Oct/1995", "Sep/1995", "Aug/1995", "Jul/1995", "Jun/1995", "May/1995"]
 count =0
 with open(r'412Project3.txt', 'r') as fp:
     lines = fp.readlines()
@@ -14,3 +6,11 @@ with open(r'412Project3.txt', 'r') as fp:
         if any(words in line for words in words):
             count += 1 
 print('Total Requests in the 6 months', count)
+
+with open(r"412Project3.txt", 'r') as fp:
+    count = 0
+    for line in fp:
+        if line != "\n":
+            count += 1
+
+print('Total Number of Lines', count)
